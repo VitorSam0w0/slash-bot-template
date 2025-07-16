c3cdc308a0b903c3170775928488dba41cdb7159
Vitor Dias <111717270+VitorSam0w0@users.noreply.github.com>
2025-07-16 14:28:53 -0300

    up

--- index.js
+++ index.js
@@ -1,24 +1,2 @@
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-
-class BotClient extends Client {
-  constructor() {
-    super({ intents: [GatewayIntentBits.Guilds] });
-    this.commands = new Collection();
-  }
-
-  async start(config) {
-    // Aqui você pode carregar comandos e eventos
-    console.log('Iniciando carregamento dos comandos...');
-
-    // Exemplo: carregar comandos (você pode adaptar para sua estrutura)
-    // await this.loadCommands();
-
-    this.once('ready', () => {
-      console.log(`Bot online! Usuário: ${this.user.tag}`);
-    });
-
-    await this.login(config.TOKEN);
-  }
-}
-
-module.exports = new BotClient();
+const config = require("./src/Configs/config.js");
+require("./src/Structures/client.js").start(config);
