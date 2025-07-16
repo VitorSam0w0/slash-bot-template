e798d38a6b24243283b255596aec6217ffeba24d
Vitor Dias <111717270+VitorSam0w0@users.noreply.github.com>
2025-07-16 14:19:29 -0300

    up

--- src/Commands/Developer/add.js
+++ src/Commands/Developer/add.js
@@ -31,50 +31,58 @@ module.exports = {
       },
     ],
   },
+
   async execute(interaction, client) {
     const { TOKEN } = client.config;
     const guild = interaction.guild;
     const rest = new REST({ version: "10" });
     rest.setToken(TOKEN);
-    // get the input (command name)
+
     const name = interaction.options.getString("name").toUpperCase();
-    // get the type of the command guild/global
     const type = interaction.options.getString("type").toUpperCase();
-    // get the command
     const command = client.commands.get(name.toLowerCase());
-    console.log(command);
+
     if (!command) {
-      return interaction.reply({
+      return await interaction.reply({
         embeds: [
           {
             title: `${name} ⊗ NOT FOUND in the FILE`,
-            color: 0xd8303b, // = #D8303B = red
+            color: 0xd8303b,
           },
         ],
         ephemeral: true,
       });
     }
-    // register the command globally, else in a guild
-    if (type === "GLOBAL") {
-      await rest.post(Routes.applicationCommands(client.user.id), {
-        body: command.data,
+
+    try {
+      if (type === "GLOBAL") {
+        await rest.post(Routes.applicationCommands(client.user.id), {
+          body: command.data.toJSON(), // IMPORTANTE usar toJSON()
+        });
+      } else {
+        await rest.post(
+          Routes.applicationGuildCommands(client.user.id, guild.id),
+          {
+            body: command.data.toJSON(),
+          }
+        );
+      }
+
+      await interaction.reply({
+        embeds: [
+          {
+            title: `${name} ⊕ ADDED to ${type}`,
+            color: 0x00ffaa,
+          },
+        ],
+        ephemeral: true,
       });
-    } else {
-      await rest.post(
-        Routes.applicationGuildCommands(client.user.id, guild.id),
-        {
-          body: command.data,
-        }
-      );
+    } catch (error) {
+      console.error("Erro ao adicionar comando:", error);
+      await interaction.reply({
+        content: "❌ Ocorreu um erro ao adicionar o comando.",
+        ephemeral: true,
+      });
     }
-    interaction.reply({
-      embeds: [
-        {
-          title: `${name} ⊕ ADDED to ${type}`,
-          color: 0x00ffaa, // = #00FFAA = green
-        },
-      ],
-      ephemeral: true,
-    });
   },
 };
